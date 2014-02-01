e921d41872ae22ad5848b09796474213652974c1
Cory G Watson <gphat@onemogin.com>
2014-02-01 11:09:26 -0800

    Docs docs docs

--- src/main/scala/io/keen/client/scala/Client.scala
+++ src/main/scala/io/keen/client/scala/Client.scala
@@ -11,26 +11,61 @@ import java.nio.charset.StandardCharsets
 // XXX These should probably be Options with handling for missing ones below.
 class Client(apiURL: String = "https://api.keen.io", version: String = "3.0", masterKey: String, writeKey: String, readKey: String) extends Logging {
 
+  /**
+   * Publish a single event. See [[https://keen.io/docs/api/reference/#event-collection-resource Event Collection Resource]].
+   *
+   * @param projectID The project to which the event will be added.
+   * @param collection The collection to which the event will be added.
+   * @param event The event
+   */
   def addEvent(projectId: String, collection: String, event: String): Future[Response] = {
     val freq = (url(apiURL) / version / "projects" / projectId / "events" / collection).secure.setBody(event.getBytes(StandardCharsets.UTF_8))
     doRequest(freq.POST, writeKey)
   }
 
+  /**
+   * Publish multiple events. See [[https://keen.io/docs/api/reference/#event-resource Event Resource]].
+   *
+   * @param projectID The project to which the event will be added.
+   * @param events The events to add to the project.
+   */
+  def addEvents(projectId: String, events: String): Future[Response] = {
+    val freq = (url(apiURL) / version / "projects" / projectId / "events").secure.setBody(events.getBytes(StandardCharsets.UTF_8))
+    doRequest(freq.POST, writeKey)
+  }
+
+  /**
+   * Returns schema information for all the event collections in this project. See [[https://keen.io/docs/api/reference/#event-resource Event Resource]].
+   *
+   * @param projectID The project to which the event will be added.
+   */
   def getEvents(projectId: String): Future[Response] = {
     val freq = (url(apiURL) / version / "projects" / projectId / "events").secure
     doRequest(freq.GET, masterKey)
   }
 
+  /**
+   * Returns available schema information for this event collection, including properties and their type. It also returns links to sub-resources. See [[https://keen.io/docs/api/reference/#event-collection-resource Event Collection Resource]].
+   *
+   * @param projectID The project to which the event will be added.
+   * @param collection The name of the collection.
+   */
   def getCollection(projectId: String, collection: String): Future[Response] = {
     val freq = (url(apiURL) / version / "projects" / projectId / "events" / collection).secure
     doRequest(freq.GET, masterKey)
   }
 
+  /**
+   * Returns the projects accessible to the API user, as well as links to project sub-resources for discovery. See [[https://keen.io/docs/api/reference/#projects-resource Projects Resource]].
+   */
   def getProjects: Future[Response] = {
     val freq = (url(apiURL) / version / "projects").secure
     doRequest(freq.GET, masterKey)
   }
 
+  /**
+   * Returns the projects accessible to the API user, as well as links to project sub-resources for discovery. See [[https://keen.io/docs/api/reference/#project-row-resource Project Row Resource]].
+   */
   def getProject(projectId: String): Future[Response] = {
     val freq = (url(apiURL) / version / "projects" / projectId).secure
     doRequest(freq.GET, masterKey)
