4ffa89b7496f3f7d8ddd8af7f6068a03740c05f5
Cory G Watson <gphat@keen.io>
2014-03-28 16:02:21 -0700

    Checkpointing. Was sitting my dir, uncommitted. :/

--- src/main/scala/io/keen/client/scala/HttpAdapter.scala
+++ src/main/scala/io/keen/client/scala/HttpAdapter.scala
@@ -5,10 +5,10 @@ import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import grizzled.slf4j.Logging
+import java.util.concurrent.TimeUnit
 import scala.concurrent.Future
 import spray.can.Http
-import spray.http.HttpMethods._
-import spray.http.{HttpRequest,HttpResponse,Uri}
+import spray.http._
 import spray.http.Uri._
 import spray.http.HttpHeaders.RawHeader
 import spray.httpx.RequestBuilding._
@@ -17,7 +17,7 @@ class HttpAdapter() extends Logging {
 
   implicit val system = ActorSystem()
   import system.dispatcher // execution context for futures
-  implicit val timeout = Timeout(10)
+  implicit val timeout = Timeout(10, TimeUnit.SECONDS)
 
   /**
    * Perform the request with some debugging for good measure.
@@ -46,13 +46,13 @@ class HttpAdapter() extends Logging {
     val finalUrl = Uri(
       scheme = scheme,
       authority = Authority(host = Host(authority)),
-      path = Path(path),
+      path = Path("/" + path),
       query = Query(filteredParams)
     )
 
     // Use the provided
     val httpMethod: HttpRequest = method match {
-      case "DELETE" => Delete(finalUrl, body)
+      case "DELETE" => HttpRequest(method = HttpMethods.DELETE, uri = finalUrl, entity = body)
       case "GET" => Get(finalUrl, body)
       case "POST" => Post(finalUrl, body)
       case _ => throw new IllegalArgumentException("Unknown HTTP method: " + method)
@@ -60,7 +60,7 @@ class HttpAdapter() extends Logging {
 
     debug("%s: %s".format(method, finalUrl))
     (IO(Http) ? httpMethod.withHeaders(
-      RawHeader("Content-type", "application/json; charset=utf-8"),
+      RawHeader("Content-type", "application/json"),
       RawHeader("Authorization", key)
     ))
       .mapTo[HttpResponse].map({ res =>
